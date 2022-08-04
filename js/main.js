bd7afbadb2ea6cbb6b7ad9cf21dda9745ecc2805
Alvaro Japa Salazar <alvaruwiiu@hotmail.com>
2022-08-04 01:12:16 -0500

    education, work and porfolio

--- js/main.js
+++ js/main.js
@@ -13,3 +13,49 @@ if(navClose){
         navMenu.classList.remove('show-menu')
     })
 }
+
+// mobile menu
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// skills
+const skillsContent = document.getElementsByClassName('skills__content')
+const skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i< skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
+
+const tabs = document.querySelectorAll('[data-target')
+const tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', ()=> {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
