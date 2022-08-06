8ab04e46fe92250c074074a744387d583adb32c8
Alvaro Japa Salazar <alvaruwiiu@hotmail.com>
2022-08-06 01:26:57 -0500

    portafolio terminado

--- js/main.js
+++ js/main.js
@@ -61,7 +61,7 @@ tabs.forEach(tab => {
 })
 
 
-let swiper = new Swiper('.porfolio__container', {
+let swiper = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
     navigation: {
@@ -72,4 +72,50 @@ let swiper = new Swiper('.porfolio__container', {
         el: '.swiper-pagination',
         clickable: true
     }
+})
+
+// scroll section active link
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' +sectionId +']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' +sectionId +']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+// Dark / Light THeme 
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bxs-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bxs-moon' : 'bxs-sun'
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bxs-sun' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
 })
